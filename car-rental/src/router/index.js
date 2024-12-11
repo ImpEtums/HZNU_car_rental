c12719977f0319f11f3efa4c7f94e31bbbaa5a90
tffang <145011335+tffang@users.noreply.github.com>
2024-12-11 16:48:38 +0800

    更新路由

--- car-rental/src/router/index.js
+++ car-rental/src/router/index.js
@@ -5,9 +5,10 @@ const routes = [
   { path: '/about', component: () => import('../components/About.vue') }, // 关于我们
   { path: '/contact', component: () => import('../components/Contact.vue') }, // 联系我们
   { path: '/login', component: () => import('../components/Login.vue') }, // 登录页面
-  { path: '/register', component: () => import('../components/Register.vue') } // 注册页面
+  { path: '/register', component: () => import('../components/Register.vue') }, // 注册页面
+  { path: '/my-orders', component: () => import('../components/MyOrders.vue') }, // 我的订单页面
+  { path: '/profile', component: () => import('../components/Profile.vue') } // 个人信息页面
 ];
-
 
 const router = createRouter({
   history: createWebHistory(),
